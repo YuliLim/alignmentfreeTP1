405ce8ddb5a418aea879676dcc4df2e777dcbdab
Yuliya Lim <yuliya@macbook-air-1.home>
2023-12-22 22:23:29 +0100

    TP1

--- kmers.py
+++ kmers.py
@@ -14,7 +14,29 @@ def kmer2str(val, k):
     str_val.reverse()
     return "".join(str_val)
 
+# A = 00, C = 01, T = 10, G = 11
+let_val = {"A": 0, "C": 1, "T": 2, "G": 3}
+rev_let_val = {"A": 2, "C": 3, "T": 0, "G": 1}
 
 def stream_kmers(text, k):
-    # --- To complete ---
-    pass
+    kmer = 0
+    rkmer = 0   
+    for letter in text[:k-1]:
+        # Forward kmer
+        kmer <<= 2
+        kmer += let_val[letter]
+        rkmer >>= 2
+        rev_letter_value = rev_let_val[letter]
+        rkmer += rev_letter_value << (2 * (k - 1))
+        
+    mask = (1 << (2 * k)) - 1    
+    for letter in text[k-1:]:
+        kmer <<= 2
+        kmer += let_val[letter]
+        kmer &= mask
+        # Reverse kmer
+        rkmer >>= 2
+        rev_letter_value = rev_let_val[letter]
+        rkmer += rev_letter_value << (2 * (k - 1))
+
+        yield kmer, rkmer
--- main.py
+++ main.py
@@ -1,14 +1,52 @@
 from loading import load_directory
-from kmers import stream_kmers, kmer2str
-
-
+from kmers import stream_kmers
+from collections import Counter
 
 def jaccard(fileA, fileB, k):
     j = 0
-    # --- To complete ---
+    
+    # Store k-mers of fileA in a dictionary
+    index_fileA = index_file(fileA, k)
+
+    # Find k-mers unique for fileA, unique for fileB and in intersection of the two
+    A_only, A_and_B, B_only = intersect_index(index_fileA, fileB, k)
+
+    # Jaccard index: intersection/ (|A| + |B| - intersection)
+    j = A_and_B / (A_only + A_and_B + B_only)
+
     return j
 
+def list_kmers(sequences, k):
+    kmers = []
+    for seq in sequences:
+        kmers.extend([min(kmer, rkmer) for kmer, rkmer in stream_kmers(seq, k)])
+    return kmers
 
+def index_file(sequences, k):
+    return Counter(list_kmers(sequences, k))
+
+def intersect_index(index, sequences, k):
+    """ Create an index containing all the kmers of the input sequences
+    """
+    index_uniq = index.copy()
+    query_uniq = 0
+    intersection = 0
+
+    for seq in sequences:
+        for kmer, rkmer in stream_kmers(seq, k):
+            minmer = min(kmer, rkmer)
+
+            # Query not in index
+            if minmer not in index_uniq:
+                query_uniq += 1
+            # Query in index => intersection
+            else:
+                intersection += 1
+                index_uniq[minmer] -= 1
+                if index_uniq[minmer] == 0:
+                    del index_uniq[minmer]
+
+    return sum(index_uniq.values()), intersection, query_uniq
 
 if __name__ == "__main__":
     # Load all the files in a dictionary
@@ -17,9 +55,6 @@ if __name__ == "__main__":
     
     filenames = list(files.keys())
     for i in range(len(files)):
-        for j in range(i+1, len(files)):
-            
-            # --- Complete here ---
-
-            j = jaccard(files[filenames[i]], files[filenames[j]], k)
-            print(filenames[i], filenames[j], j)
+        for j in range(i+1, len(files)):       
+            jac = jaccard(files[filenames[i]], files[filenames[j]], k)
+            print(filenames[i], filenames[j], jac)
